6f7d3e66a6fbc974f68ea13a96d2956733e2878d
Guadalupe Vazquez <guvadlt@gmail.com>
2021-02-10 20:08:23 -0600

    Se agrego la sumatoria de numeros con limite

--- src/PruebaRecursividad.scala
+++ src/PruebaRecursividad.scala
@@ -1,6 +1,5 @@
 
 /*
-   1) Programa que mueste la sumatoria de numeros enteros desde un limite inicial hasta un limite final
    2) Programa que muestre los DIVISORES de un numero dado
    3) Programa que muestre el FACTORIAL de un numero dado
    4) Programa que muestre los cocientes enteros de la division entre entre dos números dados
@@ -12,10 +11,19 @@ import scala.io.StdIn._
 
 object PruebaRecursividad {
   
+  // 1) Metodo para la sumatoria de numeros enteros desde un limite inicial hasta un limite final
+  def sumatoriaConLimite(limiteInicial:Int, limiteFinal:Int): Int ={
+    if(limiteFinal == limiteInicial)
+      limiteInicial
+    else
+      limiteFinal + sumatoriaConLimite(limiteInicial,limiteFinal-1)
+  }
+  
   
   def main(args: Array[String]): Unit = {
     var menu=0
     while(menu<8){
+      //Impresion de menu
       println("-----------------MENU-------------")
       println("Selecciona una opcion...")
       println("1) Sumatoria de numeros de un limite inicial a un limite final")
@@ -27,6 +35,19 @@ object PruebaRecursividad {
       println("7) Cantidad de vocales de una palabra")
       println("8) Salir")
       menu = readInt()
+      
+      //Evaluacion de las opciones
+      
+      //Opcion 1
+      if(menu==1){
+        println("Ingresa el limite inicial: ")
+        val limInicial=readInt()
+         println("Ingresa el limite final: ")
+        val limFinal=readInt()
+        println("Resultado: " + sumatoriaConLimite(limInicial,limFinal))
+      }
+      
+      
       
     }
   }
